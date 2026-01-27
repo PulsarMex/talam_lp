e0dfcfde8820ba4b053263b9c1a67889d0cf575b
DEDC <dedc_011596@hotmail.com>
2026-01-27 00:18:45 -0600

    Se detalla diseño general, se agregan los textos a los equipos y se mejora el SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,26 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  // Cambia al dominio final cuando lo tengas:
-  // site: "https://talam.com.mx",
-
+  site: "https://talam.com.mx",
   output: "static",
+  
+  compressHTML: true,
+  
+  integrations: [
+    sitemap({
+      changefreq: "monthly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+    },
   },
 });
